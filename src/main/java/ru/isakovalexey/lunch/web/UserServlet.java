03a4fec49a8cf04613fe27db914a645b8344cd6b
petrovale <leha.isakov@yandex.ru>
2017-05-29 22:35:23 +0300

    Logback

--- src/main/java/ru/isakovalexey/lunch/web/UserServlet.java
+++ src/main/java/ru/isakovalexey/lunch/web/UserServlet.java
@@ -1,20 +1,28 @@
 package ru.isakovalexey.lunch.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 /**
  * Created by user on 29.05.2017.
  */
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
+    
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
