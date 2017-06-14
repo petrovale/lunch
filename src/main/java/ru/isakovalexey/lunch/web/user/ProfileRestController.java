04c7e8c8e462000cfc0d848c0b9c4bf7351742a4
petrovale <leha.isakov@yandex.ru>
2017-06-14 11:31:25 +0300

    ProfileRestController_and_testSoapui

--- src/main/java/ru/isakovalexey/lunch/web/user/ProfileRestController.java
+++ src/main/java/ru/isakovalexey/lunch/web/user/ProfileRestController.java
@@ -1,10 +1,38 @@
 package ru.isakovalexey.lunch.web.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.isakovalexey.lunch.AuthorizedUser;
+import ru.isakovalexey.lunch.model.User;
+import ru.isakovalexey.lunch.service.UserService;
 
 /**
  * Created by user on 31.05.2017.
  */
-@Controller
-public class ProfileRestController {
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @Autowired
+    public ProfileRestController(UserService service) {
+        super(service);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(AuthorizedUser authorizedUser) {
+        return super.get(authorizedUser.id());
+    }
+
+    @DeleteMapping
+    public void delete() {
+        super.delete(AuthorizedUser.id());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user) {
+        super.update(user, AuthorizedUser.id());
+    }
 }
